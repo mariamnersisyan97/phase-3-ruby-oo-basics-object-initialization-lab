08353b955360040c0712a088322364ed9c9c12e6
Mariam Nersisyan <mariamnersisyan97@gmail.com>
2022-06-29 17:13:51 -0400

    commit tree 2ca37e861272bcf22338f17c122bb700c8e7aec5

---
+++ lib/person.rb
@@ -0,0 +1,8 @@
+class Person
+    attr_reader :name
+    def initialize(name)
+        @name = name
+    end
+end
+
+# Define a Person class in lib/person.rb. In the class, define an #initialize method that accepts an argument for the person's name. That argument should be stored within a @name instance variable.
